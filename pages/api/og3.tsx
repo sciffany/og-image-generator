c9e959b6def9b835d9a175ca84535ac030856583
Tiffany Chong <sciffany@gmail.com>
2023-01-29 21:28:12 +0800

    Commit og3

--- pages/api/og3.tsx
+++ pages/api/og3.tsx
@@ -6,7 +6,7 @@ export const config = {
 };
 
 const font = fetch(
-  new URL("../../assets/FreeSansBold.ttf", import.meta.url)
+  new URL("../../assets/RobotoCondensed-Bold.ttf", import.meta.url)
 ).then((res) => res.arrayBuffer());
 
 const WIDTH = 1200;
@@ -19,31 +19,159 @@ export default async function handler(req: NextRequest) {
 
   const name = searchParams.get("name");
   const imageUrl = searchParams.get("image-url");
+  const description = searchParams.get("description");
   const imageOffset = searchParams.get("image-offset");
+  const takeAppLink = searchParams.get("take-app-link");
+  const descriptionColor = searchParams.get("description-color");
   const logoUrl = searchParams.get("logo-url");
 
   if (
     !imageUrl ||
     !name ||
+    !description ||
     !imageOffset ||
-    !logoUrl
+    !takeAppLink ||
+    !descriptionColor
   ) {
     return new ImageResponse(<div>Missing Argument</div>);
   }
 
   return new ImageResponse(
     (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%", width: "100%", backgroundImage: "linear-gradient(#2D872F, #2D872F)"}}>
-      <img width="380px" src={logoUrl}/>
-      <div style={{fontSize: "60px", marginTop: "30px"}}>{name}</div>
-    </div>
+      <div
+        style={{
+          backgroundColor: "#0D1E3A",
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img
+          width="500px"
+          alt="main-image"
+          src={imageUrl ?? ""}
+          style={{
+            position: "absolute",
+            right: "0px",
+            top: (imageOffset ?? 100) + "px",
+            objectFit: "contain",
+          }}
+        />
+
+        <img
+          alt="blue-overlay"
+          width={WIDTH}
+          height={HEIGHT}
+          src="https://og-image-generator-ochre.vercel.app/blue9.png"
+          style={{
+            position: "absolute",
+            objectFit: "cover",
+          }}
+        />
+
+        {logoUrl && (
+          <img
+            alt="logo"
+            width="150px"
+            height="150px"
+            src={logoUrl}
+            style={{
+              left: "20px",
+              top: "20px",
+              borderRadius: "50%",
+              position: "absolute",
+              objectFit: "cover",
+            }}
+          />
+        )}
+
+        <div
+          id="title"
+          style={{
+            position: "absolute",
+            left: "20px",
+            top: "240px",
+            maxWidth: "750px",
+            fontSize: name.length <= 11 ? "120px" : "80px",
+            fontFamily: '"Roboto"',
+            color: "#E2CFBC",
+            backgroundColor: "#1B2D51",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+          }}
+        >
+          {name.toUpperCase()}
+        </div>
+
+        <div
+          id="bottom-left-trapezoid"
+          style={{
+            position: "absolute",
+            left: "-100px",
+            top: "480px",
+            width: "700px",
+            height: "220px",
+            transform: "skew(-30deg)",
+            background: descriptionColor,
+          }}
+        />
+
+        <div
+          style={{
+            position: "absolute",
+            left: "20px",
+            bottom: "-90px",
+            width: "550px",
+            height: "220px",
+            fontSize: "50px",
+            fontFamily: '"RobotoCondensed"',
+            color: "white",
+          }}
+        >
+          {description}
+        </div>
+
+        <div
+          id="bottom-right-trapezoid"
+          style={{
+            position: "absolute",
+            right: "-160px",
+            top: "540px",
+            width: "700px",
+            height: "220px",
+            transform: "skew(-30deg)",
+            background: "white",
+          }}
+        />
+
+        <div
+          style={{
+            position: "absolute",
+            // right: "-150px",
+            // bottom: "-110px",
+            display: "flex",
+            width: "1200",
+            height: "630",
+            fontSize: "30px",
+            fontFamily: '"RobotoCondensed"',
+            padding: "20px",
+            alignItems: "flex-end",
+            justifyContent: "flex-end",
+          }}
+        >
+          {takeAppLink}
+        </div>
+      </div>
     ),
     {
       width: WIDTH,
       height: HEIGHT,
       fonts: [
         {
-          name: "Arial",
+          name: "RobotoCondensed",
           data: fontData,
           style: "normal",
         },
